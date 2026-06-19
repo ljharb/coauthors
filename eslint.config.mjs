e39c67ad57b9de9c9d627ca8ac729d1472d152b7
Jordan Harband <ljharb@gmail.com>
2026-06-18 22:42:17 -0700

    [Dev Deps] update `eslint`

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import ljharb from '@ljharb/eslint-config/flat/node/20';
+
+export default [
+	...ljharb,
+	{
+		rules: {
+			complexity: 'off',
+			'max-lines-per-function': 'off',
+			'max-statements': 'off',
+			'no-magic-numbers': 'off',
+			'sort-keys': 'off',
+		},
+	},
+];
